a713e9dd8a915093c2a30fcf74bb0cf2911abace
Ismael Nunes <ismaeldenunes@gmail.com>
2025-04-10 05:10:40 -0300

    [FIX] Initial lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'dashboard',
+        title: "SyncTicket | Relatórios",
+        loadComponent: () => import('./features/dashboard/dashboard-screen/dashboard-screen.component').then(m => m.DashboardScreenComponent)
+    },
+    {
+        path: 'chat',
+        title: 'SyncTicket | Chat',
+        loadComponent: () => import('./features/chat/screen/chat-screen/chat-screen.component').then(c => c.ChatScreenComponent)
+    }
+];

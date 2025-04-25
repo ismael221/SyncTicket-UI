57030f2dbf9bd21f581e8cb824bc320c1c538980
Ismael Nunes <ismaeldenunes@gmail.com>
2025-04-25 03:33:09 -0300

    [FEAT] Electron build script

---
+++ electron/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  const indexPath = path.join(__dirname, '../dist/sync-ticket/browser/index.html');
+  win.loadURL(`file://${indexPath}`);
+
+  // Ativa o DevTools pra ver erros no console
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
